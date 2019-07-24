e9f1b145b615ceb3230df8ba1cd959b7aa88af41
zhoujincheng <zhoujincheng777@qq.com>
2019-07-24 17:02:58 +0800

    chapter5:广播最佳实践——实现强制下线功能

---
+++ BroadcastBestPractice/app/src/main/java/com/zzrenfeng/jenkin/broadcastbestpractice/BaseActivity.java
@@ -0,0 +1,61 @@
+package com.zzrenfeng.jenkin.broadcastbestpractice;
+
+import android.content.*;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    private ForceOfflineReceiver receiver;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ActivityCollector.addActivity(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction("com.zzrenfeng.jenkin.broadcastbestpractice.FORCE_OFFLINE");
+        receiver = new ForceOfflineReceiver();
+        registerReceiver(receiver, intentFilter);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(null != receiver) {
+            unregisterReceiver(receiver);
+            receiver = null;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        ActivityCollector.removeActivity(this);
+    }
+
+
+    class ForceOfflineReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(final Context context, Intent intent) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(context);
+            builder.setTitle("Warning");
+            builder.setMessage("You are forced to be offline. Please try to login again.");
+            builder.setCancelable(false);
+            builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ActivityCollector.finishAll();  //强制下线时，销毁所有活动
+                    Intent intent = new Intent(context, LoginActivity.class);
+                    context.startActivity(intent);  //强制下线后，重新启动LoginActivity
+                }
+            });
+            builder.show();
+        }
+    }
+}
