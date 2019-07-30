757504309ca9b94d71c638e2ddf42865828cbd71
zhoujincheng <zhoujincheng777@qq.com>
2019-07-30 10:35:25 +0800

    chapter8:通知的进阶技巧，设置通知铃声、震动方式、LED灯闪烁方式等

--- NotificationTest/app/src/main/java/com/zzrenfeng/jenkin/notificationtest/MainActivity.java
+++ NotificationTest/app/src/main/java/com/zzrenfeng/jenkin/notificationtest/MainActivity.java
@@ -5,11 +5,15 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -36,7 +40,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         .setSmallIcon(R.mipmap.ic_launcher)
                         .setLargeIcon(BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher))
                         .setContentIntent(pi)
-//                        .setAutoCancel(true)  //方法一：点击查看通知信息后自动取消通知栏图标（方法二参见NotificationActivity的line13-14行）
+                        .setAutoCancel(true)  //方法一：点击查看通知信息后自动取消通知栏图标（方法二参见NotificationActivity的line13-14行）
+                        .setSound(Uri.fromFile(new File("/system/media/audio/ringtones/Lyra.ogg")))  //设置提示声音
+                        .setVibrate(new long[] {0, 1000, 1000, 1000})    //设置有提示时震动（震动1s，静止1s，以此类推）；需要在AndroidManifest.xml中申请震动权限
+                        .setLights(Color.GREEN, 1000, 1000)    //设置LED灯闪烁，亮1s暗1s，一闪一闪
+//                        .setDefaults(NotificationCompat.DEFAULT_ALL)    //若不想进行如上3行的繁杂设置，可以使用通知的默认效果，根据当前手机环境来决定播放什么铃声以及如何震动等
                         .build();
                 manager.notify(1, notification);
                 break;
--- NotificationTest/app/src/main/java/com/zzrenfeng/jenkin/notificationtest/NotificationActivity.java
+++ NotificationTest/app/src/main/java/com/zzrenfeng/jenkin/notificationtest/NotificationActivity.java
@@ -10,7 +10,7 @@ public class NotificationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.notification_layout);
-        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        manager.cancel(1);    //方法二：点击通知后取消通知栏图标，此方法为显示调用；其中id=1表示为这条通知设置的id，此id在MainActivity的line41行设置；（方法一参见MainActivity的line39行）
+//        NotificationManager manager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+//        manager.cancel(1);    //方法二：点击通知后取消通知栏图标，此方法为显示调用；其中id=1表示为这条通知设置的id，此id在MainActivity的line41行（manager.notify(1, notification);）设置；（方法一参见MainActivity的line39行）
     }
 }
