3c271b910dcfe73414a6e2895d5346390d49f1d7
zhoujincheng <zhoujincheng777@qq.com>
2019-08-01 11:27:20 +0800

    chapter9:使用网络技术——解析XML格式数据：使用Pull解析方式

--- NetworkTest/app/src/main/java/com/zzrenfeng/jenkin/networktest/MainActivity.java
+++ NetworkTest/app/src/main/java/com/zzrenfeng/jenkin/networktest/MainActivity.java
@@ -2,18 +2,23 @@ package com.zzrenfeng.jenkin.networktest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
 
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private static final String TAG = "MainActivity";
 
     TextView responseText;
 
@@ -94,11 +99,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 try {
                     OkHttpClient client = new OkHttpClient();
                     Request request = new Request.Builder()
-                            .url("https://www.baidu.com")
+                            //指定访问服务器地址是电脑本机的资源url（查看方法：打开cmd，输入：adb devices 查看模拟设备列表；然后输入：adb -s [emulator-5554] shell 进入指定设备shell界面；再输入：getprop 获取设备信息；其中：[net.gprs.local-ip]: [10.0.2.15] 为本模拟手机IP；[net.eth0.gw]: [10.0.2.2]为电脑本机的IP；）
+                            .url("http://10.0.2.2:8080/get_data.xml")
                             .build();
                     Response response = client.newCall(request).execute();
                     String responseData = response.body().string();
-                    showResponse(responseData);
+                    showResponse(responseData);    //将返回数据展示在TextView中
+                    parseXMLWithPull(responseData);  //使用XMLPull解析器来解析XML
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
@@ -118,6 +125,52 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 responseText.setText(response);
             }
         });
+    }
+
+    /**
+     * 使用XMLPull解析器来解析XML
+     * @param xmlData
+     */
+    private void parseXMLWithPull(String xmlData) {
+        try {
+            XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+            XmlPullParser xmlPullParser = factory.newPullParser();
+            xmlPullParser.setInput(new StringReader(xmlData));
+            int eventType = xmlPullParser.getEventType();
+            String id = "";
+            String name = "";
+            String version = "";
+            while (eventType != XmlPullParser.END_DOCUMENT) {
+                String nodeName = xmlPullParser.getName();
+                switch (eventType) {
+                    //开始解析某个节点
+                    case XmlPullParser.START_TAG: {
+                        if("id".equals(nodeName)) {
+                            id = xmlPullParser.nextText();
+                        } else if("name".equals(nodeName)) {
+                            name = xmlPullParser.nextText();
+                        } else if("version".equals(nodeName)) {
+                            version = xmlPullParser.nextText();
+                        }
+                        break;
+                    }
+                    //完成解析某个节点，打印解析信息
+                    case XmlPullParser.END_TAG: {
+                        if("app".equals(nodeName)) {
+                            Log.d(TAG, "========[XmlPullParser]>>>> id is: " + id);
+                            Log.d(TAG, "========[XmlPullParser]>>>> name is: " + name);
+                            Log.d(TAG, "========[XmlPullParser]>>>> version is: " + version);
+                        }
+                        break;
+                    }
+                    default:
+                        break;
+                }
+                eventType = xmlPullParser.next();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
