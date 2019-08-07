1713cc4bd65a131865e2685197c7189a888b7dcc
zhoujincheng <zhoujincheng777@qq.com>
2019-08-07 11:41:44 +0800

    chapter12:Material Design实战——悬浮按钮FloatingActionButton的使用

--- MaterialTest/app/src/main/java/com/zzrenfeng/jenkin/materialtest/MainActivity.java
+++ MaterialTest/app/src/main/java/com/zzrenfeng/jenkin/materialtest/MainActivity.java
@@ -1,6 +1,7 @@
 package com.zzrenfeng.jenkin.materialtest;
 
 import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
@@ -10,6 +11,7 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -24,8 +26,8 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
-
         NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
         ActionBar actionBar = getSupportActionBar();
         if(null != actionBar) {
@@ -39,6 +41,13 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 mDrawerLayout.closeDrawers();  //点击菜单项时将滑动菜单关闭
                 return true;
+            }
+        });
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "You clicked the FloatingActionButton.", Toast.LENGTH_SHORT).show();
             }
         });
     }
