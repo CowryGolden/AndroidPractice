2f1bd885c222b436f97002a374c85735fe138639
zhoujincheng <zhoujincheng777@qq.com>
2019-08-07 11:10:34 +0800

    chapter12:Material Design实战——滑动菜单：NavigationView控件的使用

--- MaterialTest/app/src/main/java/com/zzrenfeng/jenkin/materialtest/MainActivity.java
+++ MaterialTest/app/src/main/java/com/zzrenfeng/jenkin/materialtest/MainActivity.java
@@ -1,5 +1,7 @@
 package com.zzrenfeng.jenkin.materialtest;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBar;
@@ -22,11 +24,23 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+
+        NavigationView navView = (NavigationView) findViewById(R.id.nav_view);
+
         ActionBar actionBar = getSupportActionBar();
         if(null != actionBar) {
             actionBar.setDisplayHomeAsUpEnabled(true);  //让导航按钮显示；在Toolbar最左侧的按钮叫作HomeAsUp按钮，它默认的图标是一个返回的箭头，意为返回上一个活动；这里我们对它默认的样式和作用都进行了修改；
             actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);  //设置导航按钮图标
         }
+
+        navView.setCheckedItem(R.id.nav_call);  //将Call菜单设置为默认选中
+        navView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                mDrawerLayout.closeDrawers();  //点击菜单项时将滑动菜单关闭
+                return true;
+            }
+        });
     }
 
     /**
