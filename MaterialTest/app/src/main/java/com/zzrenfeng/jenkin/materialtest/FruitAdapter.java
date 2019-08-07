3d67e434a72ab25b79cd58ea109999c085a05c98
zhoujincheng <zhoujincheng777@qq.com>
2019-08-07 17:53:12 +0800

    chapter12:Material Design实战——可折叠的标题栏(CollapsingToolbarLayout的使用)，实现根据滑动Toolbar的可折叠效果

--- MaterialTest/app/src/main/java/com/zzrenfeng/jenkin/materialtest/FruitAdapter.java
+++ MaterialTest/app/src/main/java/com/zzrenfeng/jenkin/materialtest/FruitAdapter.java
@@ -1,6 +1,7 @@
 package com.zzrenfeng.jenkin.materialtest;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
@@ -47,7 +48,21 @@ public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> 
             mContext = viewGroup.getContext();
         }
         View view = LayoutInflater.from(mContext).inflate(R.layout.fruit_item, viewGroup, false);
-        return new ViewHolder(view);
+
+        final ViewHolder holder = new ViewHolder(view);
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getAdapterPosition();
+                Fruit fruit = mFruitList.get(position);
+                Intent intent = new Intent(mContext, FruitActivity.class);
+                intent.putExtra(FruitActivity.FRUIT_NAME, fruit.getName());
+                intent.putExtra(FruitActivity.FRUIT_IMAGE_ID, fruit.getImageId());
+                mContext.startActivity(intent);
+            }
+        });
+
+        return holder;
     }
 
     @Override
