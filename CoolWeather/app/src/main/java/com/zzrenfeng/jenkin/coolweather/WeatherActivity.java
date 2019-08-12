6fd5665c0e3d9654e5d971adb9ffedc96c881a87
zhoujincheng <zhoujincheng777@qq.com>
2019-08-12 10:19:56 +0800

    酷欧天气项目实战——优化活动背景与状态栏的融合

--- CoolWeather/app/src/main/java/com/zzrenfeng/jenkin/coolweather/WeatherActivity.java
+++ CoolWeather/app/src/main/java/com/zzrenfeng/jenkin/coolweather/WeatherActivity.java
@@ -50,9 +50,10 @@ public class WeatherActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if(Build.VERSION.SDK_INT >= 21) {
+        if(Build.VERSION.SDK_INT >= 21) {  //Android5.0及以上系统才支持DecorView
             View decorView = getWindow().getDecorView();
-            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            //设置活动的布局显示在状态栏上面，让活动的背景图与状态栏融合到一起
+            decorView.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
             getWindow().setStatusBarColor(Color.TRANSPARENT);  //设置为透明
         }
 
