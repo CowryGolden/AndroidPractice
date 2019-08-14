d8f87765aae6fb46e4bddaa58835031be8a89739
zhoujincheng <zhoujincheng777@qq.com>
2019-08-14 09:00:03 +0800

    左右侧滑、下拉刷新等UI功能练习

---
+++ TabletUI/app/src/main/java/com/zzrenfeng/tabletui/FruitAdapter.java
@@ -0,0 +1,80 @@
+package com.zzrenfeng.tabletui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+import com.bumptech.glide.Glide;
+
+import java.util.List;
+
+/**
+ * 水果数据适配器
+ */
+public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> {
+
+    private Context mContext;
+
+    private List<Fruit> mFruitList;
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
+        CardView cardView;
+        ImageView fruitImage;
+        TextView fruitName;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            cardView = (CardView) itemView;
+            fruitImage = (ImageView) itemView.findViewById(R.id.fruit_image);
+            fruitName = (TextView) itemView.findViewById(R.id.fruit_name);
+        }
+    }
+
+    public FruitAdapter(List<Fruit> fruitList) {
+        mFruitList = fruitList;
+    }
+
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        if(null == mContext) {
+            mContext = viewGroup.getContext();
+        }
+        View view = LayoutInflater.from(mContext).inflate(R.layout.fruit_item, viewGroup, false);
+
+        final ViewHolder holder = new ViewHolder(view);
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int position = holder.getAdapterPosition();
+                Fruit fruit = mFruitList.get(position);
+                Intent intent = new Intent(mContext, FruitActivity.class);
+                intent.putExtra(FruitActivity.FRUIT_NAME, fruit.getName());
+                intent.putExtra(FruitActivity.FRUIT_IMAGE_ID, fruit.getImageId());
+                mContext.startActivity(intent);
+            }
+        });
+
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        Fruit fruit = mFruitList.get(position);
+        viewHolder.fruitName.setText(fruit.getName());
+        Glide.with(mContext).load(fruit.getImageId()).into(viewHolder.fruitImage);  //使用Glide加载并填充图片
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFruitList.size();
+    }
+
+}
---
+++ TabletUI/app/src/main/java/com/zzrenfeng/tabletui/MainActivity.java
@@ -0,0 +1,191 @@
+package com.zzrenfeng.tabletui;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+
+    private Fruit[] fruits = {
+            new Fruit("Apple", R.drawable.apple),
+            new Fruit("Banana", R.drawable.banana),
+            new Fruit("Orange", R.drawable.orange),
+            new Fruit("Watermelon", R.drawable.watermelon),
+            new Fruit("Pear", R.drawable.pear),
+            new Fruit("Grape", R.drawable.grape),
+            new Fruit("Pineapple", R.drawable.pineapple),
+            new Fruit("Strawberry", R.drawable.strawberry),
+            new Fruit("Cherry", R.drawable.cherry),
+            new Fruit("Mango", R.drawable.mango)
+        };
+
+    private List<Fruit> fruitList = new ArrayList<>();
+
+    private FruitAdapter adapter;
+
+    private SwipeRefreshLayout swipeRefreshLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navViewLeft = (NavigationView) findViewById(R.id.nav_view_left);
+        NavigationView navViewRight = (NavigationView) findViewById(R.id.nav_view_right);
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+
+        ActionBar actionBar = getSupportActionBar();
+        if(null != actionBar) {
+            actionBar.setDisplayHomeAsUpEnabled(true);  //让导航按钮显示；在Toolbar最左侧的按钮叫作HomeAsUp按钮，它默认的图标是一个返回的箭头，意为返回上一个活动；这里我们对它默认的样式和作用都进行了修改；
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);  //设置导航按钮图标
+        }
+
+        navViewLeft.setCheckedItem(R.id.nav_call);  //将Call菜单设置为默认选中
+        navViewLeft.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Toast.makeText(MainActivity.this, "You clicked the menuItem: " + menuItem.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                mDrawerLayout.closeDrawers();  //点击菜单项时将滑动菜单关闭
+                return true;
+            }
+        });
+
+        navViewRight.setCheckedItem(R.id.nav_call);  //将Call菜单设置为默认选中
+        navViewRight.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                Toast.makeText(MainActivity.this, "You clicked the menuItem: " + menuItem.getTitle().toString(), Toast.LENGTH_SHORT).show();
+                mDrawerLayout.closeDrawers();  //点击菜单项时将滑动菜单关闭
+                return true;
+            }
+        });
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Snackbar.make(v, "Data deleted", Snackbar.LENGTH_SHORT).setAction("Undo", new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Toast.makeText(MainActivity.this, "Data restored.", Toast.LENGTH_SHORT).show();
+                    }
+                }).show();
+            }
+        });
+
+        //加载水果数据及布局
+        initFruits();
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        GridLayoutManager layoutManager = new GridLayoutManager(this, 2);  //第二个参数spanCount：表示一行显示2列
+        recyclerView.setLayoutManager(layoutManager);
+        adapter = new FruitAdapter(fruitList);
+        recyclerView.setAdapter(adapter);
+
+        //实现RecyclerView下拉刷新逻辑
+        swipeRefreshLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh);
+        swipeRefreshLayout.setColorSchemeResources(R.color.colorPrimary);  //设置下拉刷新进度条颜色
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {  //设置下拉监听器
+            @Override
+            public void onRefresh() {
+                refreshFruits();  //下拉RecyclerView刷新水果列表
+            }
+        });
+
+    }
+
+    /**
+     * 加载toolbar.xml中菜单项
+     * @param menu
+     * @return
+     */
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        return true;
+    }
+
+    /**
+     * 处理按钮的点击事件
+     * @param item
+     * @return
+     */
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:  //HomeAsUp按钮的id永远是android.R.id.home。
+                mDrawerLayout.openDrawer(GravityCompat.START);  //这里openDrawer()方法中传入的Gravity参数一定要和activity_main.xml中定义的android:layout_gravity属性值一致；
+                break;
+            case R.id.backup:
+                Toast.makeText(this, "You clicked the menu of Backup.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.delete:
+                Toast.makeText(this, "You clicked the menu of Delete.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.settings:
+                Toast.makeText(this, "You clicked the menu of Settings.", Toast.LENGTH_SHORT).show();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+
+    /**
+     * 初始化要加载的水果数据（将水果数组fruits中的数据随机取出并放到List中）
+     */
+    private void initFruits() {
+        fruitList.clear();
+        for (int i = 0; i < 50; i++) {
+            Random random = new Random();
+            int index = random.nextInt(fruits.length);
+            fruitList.add(fruits[index]);
+        }
+    }
+
+    /**
+     * 下拉RecyclerView刷新水果列表
+     */
+    private void refreshFruits() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                runOnUiThread(new Runnable() {  //切换回主线程，更新UI
+                    @Override
+                    public void run() {
+                        initFruits();
+                        adapter.notifyDataSetChanged();  //通知数据发生了变化
+                        swipeRefreshLayout.setRefreshing(false);  //false-表示刷新事件结束，并隐藏刷新进度条
+                    }
+                });
+            }
+        }).start();
+    }
+
+}
